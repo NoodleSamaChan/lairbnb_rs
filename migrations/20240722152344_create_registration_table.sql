90141c2809903478ffb154e0c4756fdbee2ef376
NoodleSamaChan <lferraraccio@outlook.com>
2024-07-22 22:13:52 +0200

    created DB

--- migrations/20240722152344_create_registration_table.sql
+++ migrations/20240722152344_create_registration_table.sql
@@ -2,6 +2,6 @@
 CREATE TABLE users(
    id uuid NOT NULL,
    PRIMARY KEY (id),
-   account_name text UNIQUE NOT NULL PRIMARY KEY,
+   account_name text UNIQUE NOT NULL,
    account_password text NOT NULL
 );
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use lairbnb_rs::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind to random port");
+    run(listener)?.await
 }
